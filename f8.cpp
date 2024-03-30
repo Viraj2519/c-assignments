c30624eb46ccf5689297c88663a2a69b3d4e7ca5
Viraj2519 <Krishnanachhiapatel14@gmail.com>
2024-03-29 20:34:24 -0400

    the logic for file is created but there is an error in the directory

--- f8.cpp
+++ f8.cpp
@@ -24,7 +24,25 @@ int main(int argc, char** argv) {
 	strncpy(myArgs.theText, argv[2], sizeof(myArgs.theText) - 1);
 	strncpy(myArgs.directoryPath, argv[3], sizeof(myArgs.directoryPath) - 1);
 	strncpy(myArgs.filename, argv[4], sizeof(myArgs.filename) - 1);
+	
+	FILE* file;
+	char nameAndPath[MAX_SIZE * 2];
+	sprintf(nameAndPath, "%s/%s", myArgs.directoryPath, myArgs.filename);
+	
+	file = fopen(nameAndPath, "w");
+		if (!file) {
+		printf("unable to create a the file");
+		return 0;
 		
+	}
+	for (int i = 0; i < myArgs.howMany; i++) {
+		fprintf(file, "%s\n", myArgs.theText);
+		
+	}
+	fclose(file);
+	
+	printf("file has been successfully created at the specified destination");
+	
 	return 0;
 }
 
